746c4da521974254d29322574c9ce9eb870ddc9c
C6013 <g022c6013@g.ac.jp>
2023-06-23 12:22:20 +0900

    v0.1

---
+++ spring-mcv1/src/test/java/com/example/demo/SpringMcv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
